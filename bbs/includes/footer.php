966c7cb7f795af18303c5b0cc8a3fe6fa0a1637f
GoldDeMarin <golddemarin@gmail.com>
2025-05-26 02:52:58 +0300

    Initial commit

---
+++ bbs/includes/footer.php
@@ -0,0 +1,26 @@
+
+    <div class="footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                   
+                  Boat Booking System <i class="icon-heart text-danger" aria-hidden="true"></i>
+                    </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="contact-info">
+    <h3>Dream Yachting</h3>
+    <p>📍 Cap d'Antibes & Cannes, French Riviera</p>
+    <p>📞 +33 (0)4 XX XX XX XX</p>
+    <p>✉️ info@dream-yachting.co</p>
+    <div class="social-links">
+      <a href="https://wa.me/33XXXXXXXXX">WhatsApp</a>
+      <a href="viber://chat?number=33XXXXXXXXX">Viber</a>
+      <a href="https://t.me/dreamyachting">Telegram</a>
+    </div>
+  </div>
